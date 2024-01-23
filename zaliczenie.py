508ae584b3e81c6a657b1e0ed04de0e12a32d49b
Michał Bagrowski <bagrowski0001@gmail.com>
2024-01-23 20:50:29 +0100

    zaliczenie na 4

--- zaliczenie.py
+++ zaliczenie.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from flask import Flask, Response, request
+from flask import Flask, Response, request, render_template_string
 from flask_restful import Resource, Api
 import requests
 
@@ -10,35 +10,41 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/')
+def index():
+    return render_template_string('''
+        <form action="/people-counter-url" method="get">
+            Podaj adres URL zdjęcia: <input type="text" name="image_url">
+            <input type="submit" value="wyślij">
+        </form>>
+    ''')
 
+# Część pierwsza: API, które posiada 1 endpoint GET, który czyta z dysku zdjęcie i zwraca informację
+# o tym ile znaleziono osób na zdjęciu.
 class PeopleCounter(Resource):
     def get(self):
         # ładowanie zdjęcia
-        image_path = 'G:/projects/GitTutorial/UczMasz/ludzie.jpg'
+        image_path = 'G:/PyCharm/ProjektyPython/ludzie.jpg'
         image = cv2.imread(image_path)
 
-        # sprawdzenie, czy obraz został poprawnie załadowany
+        # sprawdzenie, czy zdjęcie zostało poprawnie załadowane
         if image is None:
             return {"error": "Nie można załadować obrazu"}, 404
-        image = cv2.resize(image, (700, 400))
 
         # wykrywanie ludzi na zdjęciu
-        (rects, weights) = hog.detectMultiScale(image, winStride=(2, 2), padding=(16, 16), scale=1.01)
+        (rects, _) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-        # Rysowanie kwadratów
-        policzono_ludzi = len(rects)
-        for (x, y, w, h) in rects:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Liczenie osób na zdjęciu
+        liczba_osob = len(rects)
 
-        # Zamiana zdjęcia na jpeg
-        _, jpeg = cv2.imencode('.jpg', image)
+        # wyświetlenie odpowiedzi
+        return {"liczba_osob": liczba_osob}
 
-        # Wyświetlenie zdjęcia w odpowiedzi
-        return Response(jpeg.tobytes(), mimetype='image/jpeg')
+api.add_resource(PeopleCounter, '/people-counter')
 
-
-api.add_resource(PeopleCounter, '/')
-
+# Część druga: API, które posiada 2 endpointy, jeden z punktu a., drugi GET który w parametrze
+# otrzymuje link do zdjęcia, które jest w Internecie, pobiera je, a następnie zwraca
+# informację o tym ile znaleziono osób na zdjęciu.
 
 class PeopleCounterURL(Resource):
     def get(self):
@@ -61,19 +67,13 @@ class PeopleCounterURL(Resource):
             # wykrywanie ludzi na zdjęciu
             (rects, weights) = hog.detectMultiScale(image, winStride=(2, 2), padding=(16, 16), scale=1.01)
 
-            # Rysowanie kwadratów
-            policzono_ludzi = len(rects)
-            for (x, y, w, h) in rects:
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
             # Zwróć liczbę wykrytych osób
             return {'peopleCount': len(rects)}
 
         except Exception as e:
             return {"error": str(e)}, 500
 
-
-api.add_resource(PeopleCounterURL, '/url')
+api.add_resource(PeopleCounterURL, '/people-counter-url')
 
 if __name__ == '__main__':
     app.run(debug=True)
